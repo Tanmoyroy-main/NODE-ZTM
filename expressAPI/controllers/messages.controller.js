1b323540d0edebd797e726c9b9fe1f18d5bfd0dd
Tanmoyroy-main <1728128@kiit.ac.in>
2022-05-09 08:57:24 +0530

    hbs-Template engine implementation

--- expressAPI/controllers/messages.controller.js
+++ expressAPI/controllers/messages.controller.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 function getMessage(req, res) {
   // res.send('GET Hello from messages');
-  res.sendFile(path.join(__dirname, '..', 'public', 'img.png'));
+  // res.sendFile(path.join(__dirname, '..', 'public', 'img.png'));
+  res.render('messages', {
+    title: 'Messages to my friends!',
+    friend: 'Elon Musk',
+  });
 }
 function postMessage(req, res) {
   console.log('Updating messages');
--- expressAPI/server.js
+++ expressAPI/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const friendRouter = require('./routes/friends.router');
 const messageRouter = require('./routes/messages.router');
 
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
@@ -20,8 +23,18 @@ app.use((req, res, next) => {
   console.log(`${req.method} : ${req.baseUrl}${req.url} : ${respTime}ms`);
 });
 
+//below line helps to host our website on node
+app.use('/site', express.static(path.join(__dirname, 'public')));
+
 //The below middleware will set the request as json instead of object so that it can be used in the next post req to friends below.
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Roy',
+    caption: 'Implementing templating engine in Express',
+  });
+});
 
 app.use('/friends', friendRouter);
 app.use('/messages', messageRouter);
